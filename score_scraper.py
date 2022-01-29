2c5fd8833cc2f485789ec6fb436326b0bcca3a0f
Karueo <nicobgw25@gmail.com>
2022-01-29 01:19:48 -0500

    replaced file input with url input

--- score_scraper.py
+++ score_scraper.py
@@ -1,20 +1,27 @@
 from bs4 import BeautifulSoup
 import csv
 from html.parser import HTMLParser
-import os
+import requests
 
 def makeReadable(soup, newFilename):
     newFile = open(newFilename, "w+")
     newFile.write(soup.prettify())
     return newFile
 
-file = open('original.htm', "r+")
-soup = BeautifulSoup(file.read(), "html.parser")
+url = "https://tv.varsity.com/results/7379346-2022-athletic-providence-grand-national-didii/31423"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
+# file = open('original.htm', "r+")
+# soup = BeautifulSoup(file.read(), "html.parser")
 #file = makeReadable(soup, "updated.htm")
 
 scores = soup.find_all(class_ = "full-content")
 # currently, go to third child for tables
 # TODO add discerning btwn days 1 and 2
+
+if len(scores) < 3:
+    print("No scores available")
+    exit()
 
 days = 1
 csvfile = open("scores.csv", "w")
@@ -28,7 +35,7 @@ numRows = 0
 # TODO Fix printing day 1 and day 2
 csvwriter.writerow(divisionQueue.pop(0)) # print Level
 for data in scores:
-    if data.previous.previous == "Day 2": # close, perhaps check previous elements for patterns
+    if data.previous.previous == "Day 2" or "Day" not in data.previous.previous: # close, perhaps check previous elements for patterns
         csvwriter.writerow(divisionQueue.pop(0))
     for row in data.contents[numRows:]:
         fields = []
