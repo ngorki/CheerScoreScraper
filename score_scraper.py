3997d7dac139be11ba8634eedb682ea5674e67a7
Karueo <nicobgw25@gmail.com>
2022-01-29 02:03:04 -0500

    added feature: parsing all levels

--- score_scraper.py
+++ score_scraper.py
@@ -8,7 +8,27 @@ def makeReadable(soup, newFilename):
     newFile.write(soup.prettify())
     return newFile
 
-url = "https://tv.varsity.com/results/7379346-2022-athletic-providence-grand-national-didii/31423"
+def getLevelUrls(url, soup):
+    newUrls = []
+    levels = soup.find_all("option")
+    for level in levels:
+        if(url[:-5] + level['value'] != url):
+            newUrls.append(url[:-5] + level['value'])
+    return newUrls
+
+def writeToCSV(csvwriter, scores, divisionQueue):
+    if len(scores) < 3:
+        print("No scores available")
+    for data in scores:
+        if data.previous.previous == "Day 2" or "Day" not in data.previous.previous: # close, perhaps check previous elements for patterns
+            csvwriter.writerow(divisionQueue.pop(0))
+        for row in data.contents[1:]:
+            fields = []
+            for field in row.contents:
+                fields.append(field.text)
+            csvwriter.writerow(fields)
+
+url = "https://tv.varsity.com/results/7361971-2022-spirit-unlimited-battle-at-the-boardwalk-atlantic-city-grand-ntls/31220"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 # file = open('original.htm', "r+")
@@ -19,30 +39,36 @@ scores = soup.find_all(class_ = "full-content")
 # currently, go to third child for tables
 # TODO add discerning btwn days 1 and 2
 
+newUrls = getLevelUrls(url, soup)
 if len(scores) < 3:
     print("No scores available")
-    exit()
 
-days = 1
 csvfile = open("scores.csv", "w")
 csvwriter = csv.writer(csvfile)
 csvInit = True
 
-divisionQueue = scores[2]("h2")
-scores = scores[2]("tbody")
-numRows = 0
+for newUrl in newUrls:
+    response = requests.get(newUrl)
+    soup = BeautifulSoup(response.text, "html.parser")
+    scores = soup.find_all(class_ = "full-content")
+    divisionQueue = scores[2]("h2")
+    scores = scores[2]("tbody")
+    writeToCSV(csvwriter, scores, divisionQueue)
 
-# TODO Fix printing day 1 and day 2
-csvwriter.writerow(divisionQueue.pop(0)) # print Level
-for data in scores:
-    if data.previous.previous == "Day 2" or "Day" not in data.previous.previous: # close, perhaps check previous elements for patterns
-        csvwriter.writerow(divisionQueue.pop(0))
-    for row in data.contents[numRows:]:
-        fields = []
-        for field in row.contents:
-            fields.append(field.text)
-        csvwriter.writerow(fields)
-        if(numRows == 0):
-            numRows += 1
-        
-   
+if(len(scores) < 3):
+    divisionQueue = scores[2]("h2")
+    scores = scores[2]("tbody")
+    numRows = 0
+
+    # TODO Fix printing day 1 and day 2
+    csvwriter.writerow(divisionQueue.pop(0)) # print Level
+    for data in scores:
+        if data.previous.previous == "Day 2" or "Day" not in data.previous.previous: # close, perhaps check previous elements for patterns
+            csvwriter.writerow(divisionQueue.pop(0))
+        for row in data.contents[numRows:]:
+            fields = []
+            for field in row.contents:
+                fields.append(field.text)
+            csvwriter.writerow(fields)
+            if(numRows == 0):
+                numRows += 1
